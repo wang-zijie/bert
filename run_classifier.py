232352f24201aeed35a962600a53648b203db4e4
Zijie Wang <36257102+wang-zijie@users.noreply.github.com>
2020-10-02 16:12:44 -0700

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -290,51 +290,6 @@ class MnliProcessor(DataProcessor):
     return examples
 
 
-class QqpProcessor(DataProcessor):
-  """Processor for the QQP data set (GLUE version)."""
-
-  def get_train_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
-
-  def get_dev_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
-
-  def get_test_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
-
-  def get_labels(self):
-    """See base class."""
-    return ["0", "1"]
-
-  def _create_examples(self, lines, set_type):
-    """Creates examples for the traininmg and dev sets."""
-    examples = []
-    try:
-      for (i, line) in enumerate(lines):
-        if i == 0:
-          continue
-        guid = "%s-%s" % (set_type, i)
-
-        text_a = tokenization.convert_to_unicode(line[3])
-        text_b = tokenization.convert_to_unicode(line[4])
-        if set_type == "test":
-          label = "0"
-        else:
-          label = tokenization.convert_to_unicode(line[-1])
-        examples.append(
-            InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-    except:
-      print(guid)
-    return examples
-
-
-
 class MrpcProcessor(DataProcessor):
   """Processor for the MRPC data set (GLUE version)."""
 
@@ -372,7 +327,6 @@ class MrpcProcessor(DataProcessor):
         label = tokenization.convert_to_unicode(line[0])
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-      print(examples)
     return examples
 
 
@@ -831,7 +785,6 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
-      "qqp":  QqpProcessor
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
